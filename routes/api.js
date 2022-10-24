becf7b2e026ca36b2810d0695e467a8b88979396
Christian Flores <c1flores@ucsd.edu>
2022-10-23 20:54:14 -0700

    Shorten file names and add CRUD logic for api handling

---
+++ routes/api.js
@@ -0,0 +1,29 @@
+// Dependencies 
+const router = require('express').Router();
+const saveData = require('../db/saveData');
+
+// GET Request 
+router.get('/notes', function (req, res) {
+    saveData
+        .retrieveNotes()
+        .then(notes => res.json(notes))
+        .catch(err => res.status(500).json(err));
+});
+
+// POST Request
+router.post('/notes', function (req, res) {
+    saveData
+        .addNote()
+        .then(notes => res.json(notes))
+        .catch(err => res.status(500).json(err));
+});
+
+// DELETE Request
+router.delete('/notes/:id', function (req, res) {
+    saveData
+        .deleteNote(req.params.id)
+        .then(() => res.json({ok: true}))
+        .catch(err => res.status(500).json(err));
+});
+
+module.exports =  router
